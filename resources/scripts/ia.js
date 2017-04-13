881e39f6402d71211bfdc34309f88844f517585a
Xx-Johnatas-xX <sylvain.vanel@epsi.fr>
2017-04-13 14:00:45 +0200

    JS IA init

--- resources/scripts/ia.js
+++ resources/scripts/ia.js
@@ -1,5 +1,5 @@
 function premierTourIA() {
-    
+    play(9,9);
 }
 
 function deuxiemeTourIA() {
@@ -7,13 +7,32 @@ function deuxiemeTourIA() {
 }
 
 function premierTourAdversaire() {
-    
+    var tabfirst = ["8.9", "10.9", "9.8", "9.10", "8.8", "8.10", "10.8", "10.10", "7.9", "11.9", "9.7", "9.11"];
+    var randomfirst = tabfirst[Math.floor(Math.random() * tabfirst.length)];
+    var x = randomfirst.substr(0, 1);
+    var y = randomfirst.substr(2, 1);
+
+    play(x, y);
 }
 
 function deuxiemeTourAdversaire() {
-    
+
 }
 
 function autresTours() {
     
+}
+
+function play(x, y) {
+    var route = host + "/play/" + x + "/" + y + "/" + playerId;
+
+    $.ajax({
+        dataType: "json",
+        type: 'get',
+        url: route,
+        success: function (data) {
+            // data : objet JSON renvoyé par le serveur
+            console.log(data);
+        }
+    });
 }
