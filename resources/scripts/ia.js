ce33c80fd9ebdae391aaa169aec94e61904d457b
Xx-Johnatas-xX <sylvain.vanel@epsi.fr>
2017-04-13 11:28:49 +0200

    JS IA init

---
+++ resources/scripts/ia.js
@@ -0,0 +1,19 @@
+function premierTourIA() {
+    
+}
+
+function deuxiemeTourIA() {
+    
+}
+
+function premierTourAdversaire() {
+    
+}
+
+function deuxiemeTourAdversaire() {
+    
+}
+
+function autresTours() {
+    
+}
